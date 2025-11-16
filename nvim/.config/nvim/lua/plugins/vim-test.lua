e83b930cec80107cf814c97b6956786ac4db3969
Jonny Rimek <jimbo.db@protonmail.com>
2025-11-16 20:32:30 +0100

    Add two more plugin configs to be stowed

---
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,16 @@
+return {
+  "vim-test/vim-test",
+  -- dependencies = {
+  --   "preservim/vimux",
+  -- },
+  keys = {
+    { "<leader>t", "<cmd>TestNearest<CR>", desc = "Test nearest", silent = true },
+    { "<leader>T", "<cmd>TestFile<CR>", desc = "Test file", silent = true },
+    { "<leader>a", "<cmd>TestSuite<CR>", desc = "Test suite", silent = true },
+    { "<leader>l", "<cmd>TestLast<CR>", desc = "Test last", silent = true },
+    { "<leader>g", "<cmd>TestVisit<CR>", desc = "Test visit", silent = true },
+  },
+  -- config = function()
+  --   vim.cmd("let test#strategy = 'vimux'")
+  -- end,
+}
