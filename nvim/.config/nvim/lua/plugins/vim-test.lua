4c6b485900d4afd1bc1cc6737ab5f5a88d235a79
Jonny Rimek <jimbo.db@protonmail.com>
2025-11-20 19:55:16 +0100

    Add rspec document test format

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -4,13 +4,31 @@ return {
 	--   "preservim/vimux",
 	-- },
 	keys = {
-		{ "<leader>tt", "<cmd>TestNearest<CR>", desc = "Test nearest", silent = true },
-		{ "<leader>tT", "<cmd>TestFile<CR>", desc = "Test file", silent = true },
+		-- { "<leader>tt", "<cmd>TestNearest<CR>", desc = "Test nearest", silent = true },
+		{ "<leader>tt", "<cmd>TestFile<CR>", desc = "Test file", silent = true },
 		{ "<leader>ta", "<cmd>TestSuite<CR>", desc = "Test suite", silent = true },
 		{ "<leader>tl", "<cmd>TestLast<CR>", desc = "Test last", silent = true },
 		{ "<leader>tg", "<cmd>TestVisit<CR>", desc = "Test visit", silent = true },
+		{
+			"<leader>td",
+			function()
+				-- Store original options
+				local original_options = vim.g["test#ruby#rspec#options"]
+
+				-- Set documentation format
+				vim.g["test#ruby#rspec#options"] = "--format documentation"
+
+				-- Run the test
+				vim.cmd("TestFile")
+
+				-- Restore original options
+				vim.g["test#ruby#rspec#options"] = original_options
+			end,
+			desc = "Test file (documentation format)",
+			silent = true,
+		},
 	},
 	-- config = function()
-	--   vim.cmd("let test#strategy = 'vimux'")
+	-- 	vim.cmd("let test#strategy = 'vimux'")
 	-- end,
 }
