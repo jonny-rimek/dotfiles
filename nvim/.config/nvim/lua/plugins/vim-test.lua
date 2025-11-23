608e84d495f514566899c5254158ec3728884283
Jonny Rimek <jimbo.db@protonmail.com>
2025-11-23 21:08:12 +0100

    Rspec vim test changes

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -9,26 +9,9 @@ return {
 		{ "<leader>ta", "<cmd>TestSuite<CR>", desc = "Test suite", silent = true },
 		{ "<leader>tl", "<cmd>TestLast<CR>", desc = "Test last", silent = true },
 		{ "<leader>tg", "<cmd>TestVisit<CR>", desc = "Test visit", silent = true },
-		{
-			"<leader>td",
-			function()
-				-- Store original options
-				local original_options = vim.g["test#ruby#rspec#options"]
-
-				-- Set documentation format
-				vim.g["test#ruby#rspec#options"] = "--format documentation"
-
-				-- Run the test
-				vim.cmd("TestFile")
-
-				-- Restore original options
-				vim.g["test#ruby#rspec#options"] = original_options
-			end,
-			desc = "Test file (documentation format)",
-			silent = true,
-		},
 	},
-	-- config = function()
-	-- 	vim.cmd("let test#strategy = 'vimux'")
-	-- end,
+	config = function()
+		-- vim.cmd("let test#strategy = 'vimux'")
+		vim.g["test#ruby#rspec#options"] = "--format documentation"
+	end,
 }
