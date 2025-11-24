2494d8c7b4453535955601a8819d032b77c1ff4d
Jonny Rimek <jimbo.db@protonmail.com>
2025-11-24 21:32:50 +0100

    Fix shift k doing lsp stuff

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,12 +10,27 @@ pcall(vim.keymap.del, "n", "<S-k>")
 -- Disable default behaviors
 vim.keymap.set("n", "H", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "L", "<nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "J", "<nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "K", "<nop>", { noremap = true, silent = true })
 
--- Map J/K for BUFFER navigation
+-- Map J/K for BUFFER navigation (removed K from <nop> section)
 vim.keymap.set("n", "J", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
 vim.keymap.set("n", "K", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+
+-- Force K to remain bnext even after LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.schedule(function()
+			-- Delete LSP's K mapping
+			pcall(vim.keymap.del, "n", "K", { buffer = args.buf })
+			-- Re-apply our mapping
+			vim.keymap.set("n", "K", ":bnext<CR>", {
+				buffer = args.buf,
+				noremap = true,
+				silent = true,
+				desc = "Next buffer",
+			})
+		end)
+	end,
+})
 
 -- Unbind LazyVim's default lazygit keymaps
 vim.keymap.del("n", "<leader>gg")
