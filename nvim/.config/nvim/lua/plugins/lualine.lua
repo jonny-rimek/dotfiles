6b11c25e81b4572caf30ef960f41d0653076639f
Jonny Rimek <jimbo.db@protonmail.com>
2026-02-26 22:54:30 +0100

    Full path in nvim

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,12 @@ return {
 		opts = function(_, opts)
 			opts.sections.lualine_z = {}
 			opts.sections.lualine_y = {}
+
+			-- Show full path from git root, never truncate
+			-- Remove root_dir (index 1) since full path makes it redundant
+			-- Replace pretty_path (index 4) with untruncated version
+			table.remove(opts.sections.lualine_c, 1)
+			opts.sections.lualine_c[3] = { LazyVim.lualine.pretty_path({ relative = "root", length = 0 }) }
 		end,
 	},
 }
