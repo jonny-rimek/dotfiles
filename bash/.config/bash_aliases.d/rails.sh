9d8f9feb3c980e44906fc7869c5952ecf256d882
Jonny Rimek <jimbo.db@protonmail.com>
2026-02-26 21:43:43 +0100

    Add rails aliases

--- bash/.config/bash_aliases.d/rails.sh
+++ bash/.config/bash_aliases.d/rails.sh
@@ -1,7 +1,14 @@
-alias rdbm="bundle exec rails db:migrate"
-alias rr="bundle exec rails routes"
-alias rgs="bundle exec rails generate scaffold"
-alias rgm="bundle exec rails generate model"
+alias rdbm="bin/rails db:migrate"
+alias rr="bin/rails routes"
+alias rgs="bin/rails generate scaffold"
+alias rg="bin/rails generate"
+alias rgm="bin/rails generate model"
+alias rgmi="bin/rails generate migration"
 
 alias bi="bundle install"
 alias bu="bundle update"
+
+alias ci="bin/ci"
+
+alias rba="bundle exec rubocop --autocorrect"
+alias rbaa="bundle exec rubocop --autocorrect-all"
